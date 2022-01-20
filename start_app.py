d71cf1b9a3cccad98adae98bf30fad68cf8b43e3
Nikolay <n.zaretskij@gmail.com>
2022-01-20 15:05:19 +0200

    modification tools

--- start_app.py
+++ start_app.py
@@ -241,7 +241,11 @@ class ThreadStreamsCurrentValue(Thread, QtCore.QObject):
         self.pipeline.start(self.config)
         self.pix_border_w = 10
         self.pix_border_h = 10
+        self.death_width_zone =  params["death_width_percent_zone"]
+        self.min_area_roi = params["roi_area"]["min"]
+        self.max_area_roi = params["roi_area"]["max"]
         self.work_zone = (params['work_zone'][0], params['work_zone'][1], params['work_zone'][2], params['work_zone'][3])
+        self.work_zone_area = (params['work_zone'][3] - params['work_zone'][1]) * (params['work_zone'][2] - params['work_zone'][0])
         self.cam_h = params['camera']['y']
         self.cam_w = params['camera']['x']
         self.area_screen = self.cam_h * self.cam_w
@@ -302,7 +306,7 @@ class ThreadStreamsCurrentValue(Thread, QtCore.QObject):
                 cnt = cnt_sorted[0]
                 area = cv2.contourArea(cnt)
                 image_copy = np.zeros((self.cam_h, self.cam_w))
-                if self.area_screen * 0.9 < area:
+                if self.area_screen * 0.95 < area:
                     if len(cnt_sorted) > 1:
                         cnt = sorted(contours, key=len, reverse=True)[1]
                     else:
@@ -310,111 +314,114 @@ class ThreadStreamsCurrentValue(Thread, QtCore.QObject):
                 if cnt is not None:
 
                     rbox = cv2.minAreaRect(cnt)
-                    pts = cv2.boxPoints(rbox).astype(np.int32)
-                    cv2.drawContours(image_copy, [pts], -1, 255, -1)
-                    #print("centre ", rbox[0] ," w,h ", rbox[1], " angle ", rbox[2])
+                    box_area = rbox[1][0] * rbox[1][1]
+                    if self.min_area_roi * self.work_zone_area <=box_area <= self.max_area_roi * self.work_zone_area:
 
-                    if rbox[1][0] >= rbox[1][1]:
-                        ln_w = int(rbox[1][1] * 0.3)
+                        pts = cv2.boxPoints(rbox).astype(np.int32)
+                        cv2.drawContours(image_copy, [pts], -1, 255, -1)
+                        #print("centre ", rbox[0] ," w,h ", rbox[1], " angle ", rbox[2])
 
-                        angle2 = np.pi - np.radians(int(180-rbox[2]))
+                        if rbox[1][0] >= rbox[1][1]:
+                            ln_w = int(rbox[1][1] * self.death_width_zone)
 
-                        x1, y1 = int(rbox[0][0]), int(rbox[0][1])
-                        length = rbox[1][0] / 2
-                        x1_0 = int(x1 + length * np.cos(angle2))
-                        y1_0 = int(y1 + length * np.sin(angle2))
+                            angle2 = np.pi - np.radians(int(180-rbox[2]))
 
-                        angle1 = np.pi +angle2
-                        x2_0 = int(x1 + length * np.cos(angle1))
-                        y2_0 = int(y1 + length * np.sin(angle1))
+                            x1, y1 = int(rbox[0][0]), int(rbox[0][1])
+                            length = rbox[1][0] / 2
+                            x1_0 = int(x1 + length * np.cos(angle2))
+                            y1_0 = int(y1 + length * np.sin(angle2))
 
-
-                        thresh_img = cv2.line(thresh_img, (x1_0, y1_0), (x2_0, y2_0), (255, 255, 255), ln_w)
-                        image_copy = cv2.line(image_copy, (x1_0, y1_0), (x2_0, y2_0), 0, ln_w)
-
-                        w = rbox[1][0]
-                        h = rbox[1][1]
-
-                        delta_x = pts[1][0] - pts[0][0]
-                        delta_y = pts[0][1] - pts[1][1]
+                            angle1 = np.pi +angle2
+                            x2_0 = int(x1 + length * np.cos(angle1))
+                            y2_0 = int(y1 + length * np.sin(angle1))
 
 
+                            thresh_img = cv2.line(thresh_img, (x1_0, y1_0), (x2_0, y2_0), (255, 255, 255), ln_w)
+                            image_copy = cv2.line(image_copy, (x1_0, y1_0), (x2_0, y2_0), 0, ln_w)
 
-                        #print("w ",int(rbox[1][0]) ," h ", int(rbox[1][1]), "angle ", int(180-rbox[2]), "  ", angle2)
-                    else :#rbox[1][0] <rbox[1][1]:
-                        angle2= np.pi - np.radians(int(90 - rbox[2]))
-                        ln_w = int(rbox[1][0] * 0.3)
-                        x1, y1 = int(rbox[0][0]), int(rbox[0][1])
-                        length = rbox[1][1] / 2
-                        x1_0 = int(x1 + length * np.cos(angle2))
-                        y1_0 = int(y1 + length * np.sin(angle2))
+                            w = rbox[1][0]
+                            h = rbox[1][1]
 
-
-                        angle1 = np.pi +angle2
-                        x2_0 = int(x1 + length * np.cos(angle1))
-                        y2_0 = int(y1 + length * np.sin(angle1))
-
-
-                        thresh_img = cv2.line(thresh_img, (x1_0, y1_0), (x2_0, y2_0), 255, ln_w)
-                        image_copy = cv2.line(image_copy, (x1_0, y1_0), (x2_0, y2_0), 0, ln_w)
-
-                        w = rbox[1][1]
-                        h = rbox[1][0]
-
-                        delta_x = pts[2][0] - pts[1][0]
-                        delta_y = pts[2][1] - pts[1][1]
-
-                        #print("w ",int(rbox[1][0]) ," h ", int(rbox[1][1]), "angle ",int(90-rbox[2]))
-
-                    A = np.argwhere(image_copy >= 255)
-                    res = np.array([original_copy[v[0], v[1]] for v in A])
-                    res[res <= 0.1] = np.nan
-                    dist = np.nanmean(res)
-                    px_mm_x = (dist * 1000 * self.fov_x_2) / self.img_w_2
-                    px_mm_y = (dist * 1000 * self.fov_y_2) / self.img_h_2
-
-                    delta_x_mm = delta_x * px_mm_x
-                    delta_y_mm = delta_y * px_mm_y
-                    res_W = np.sqrt((delta_x_mm ** 2 + delta_y_mm ** 2))
-                    #print(res_W)
-                    self.kalman.iterative_updates(res_W)
+                            delta_x = pts[1][0] - pts[0][0]
+                            delta_y = pts[0][1] - pts[1][1]
 
 
 
+                            #print("w ",int(rbox[1][0]) ," h ", int(rbox[1][1]), "angle ", int(180-rbox[2]), "  ", angle2)
+                        else :#rbox[1][0] <rbox[1][1]:
+                            angle2= np.pi - np.radians(int(90 - rbox[2]))
+                            ln_w = int(rbox[1][0] * self.death_width_zone)
+                            x1, y1 = int(rbox[0][0]), int(rbox[0][1])
+                            length = rbox[1][1] / 2
+                            x1_0 = int(x1 + length * np.cos(angle2))
+                            y1_0 = int(y1 + length * np.sin(angle2))
+
+
+                            angle1 = np.pi +angle2
+                            x2_0 = int(x1 + length * np.cos(angle1))
+                            y2_0 = int(y1 + length * np.sin(angle1))
+
+
+                            thresh_img = cv2.line(thresh_img, (x1_0, y1_0), (x2_0, y2_0), 255, ln_w)
+                            image_copy = cv2.line(image_copy, (x1_0, y1_0), (x2_0, y2_0), 0, ln_w)
+
+                            w = rbox[1][1]
+                            h = rbox[1][0]
+
+                            delta_x = pts[2][0] - pts[1][0]
+                            delta_y = pts[2][1] - pts[1][1]
+
+                            #print("w ",int(rbox[1][0]) ," h ", int(rbox[1][1]), "angle ",int(90-rbox[2]))
+
+                        A = np.argwhere(image_copy >= 255)
+                        res = np.array([original_copy[v[0], v[1]] for v in A])
+                        res[res <= 0.1] = np.nan
+                        dist = np.nanmean(res)
+                        px_mm_x = (dist * 1000 * self.fov_x_2) / self.img_w_2
+                        px_mm_y = (dist * 1000 * self.fov_y_2) / self.img_h_2
+
+                        delta_x_mm = delta_x * px_mm_x
+                        delta_y_mm = delta_y * px_mm_y
+                        res_W = np.sqrt((delta_x_mm ** 2 + delta_y_mm ** 2))
+                        #print(res_W)
+                        self.kalman.iterative_updates(res_W)
 
 
 
 
-                    # print("pix per mm x ",px_mm_x, " pix per mm y ", px_mm_y)
-                    #print(dist, " x ", px_mm_x * w, " y ", px_mm_y * h)
 
 
-                    # font = cv2.FONT_HERSHEY_SIMPLEX
-                    # org = (int(rbox[0][0]), int(rbox[0][1]))
-                    # fontScale = 1
-                    # color = 255
-                    # thickness = 3
-                    # thresh_img = cv2.putText(thresh_img, "C", org, font,
-                    #                          fontScale, color, thickness, cv2.LINE_AA)
 
-                    for p in range(len(pts)):
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        org = (pts[p][0], pts[p][1])
-                        fontScale = 1
-                        color = 0
-                        thickness = 3
-                        thresh_img = cv2.putText(thresh_img, str(p), org, font,
-                                             fontScale, color, thickness, cv2.LINE_AA)
+                        # print("pix per mm x ",px_mm_x, " pix per mm y ", px_mm_y)
+                        #print(dist, " x ", px_mm_x * w, " y ", px_mm_y * h)
 
-                    thresh_img = cv2.drawContours(thresh_img, [pts], -1, (0, 255, 0), 1, cv2.LINE_AA)
 
-                    work_r_points = [np.array(([self.work_zone[0], self.work_zone[1]],
-                              [self.work_zone[2], self.work_zone[1]],
-                              [self.work_zone[2], self.work_zone[3]],
-                              [self.work_zone[0], self.work_zone[3]]
-                              ))]
-                    thresh_img = cv2.drawContours(thresh_img, work_r_points,
-                                                  -1, (0, 255, 0), 1, cv2.LINE_AA)
+                        # font = cv2.FONT_HERSHEY_SIMPLEX
+                        # org = (int(rbox[0][0]), int(rbox[0][1]))
+                        # fontScale = 1
+                        # color = 255
+                        # thickness = 3
+                        # thresh_img = cv2.putText(thresh_img, "C", org, font,
+                        #                          fontScale, color, thickness, cv2.LINE_AA)
+
+                        for p in range(len(pts)):
+                            font = cv2.FONT_HERSHEY_SIMPLEX
+                            org = (pts[p][0], pts[p][1])
+                            fontScale = 1
+                            color = 0
+                            thickness = 3
+                            thresh_img = cv2.putText(thresh_img, str(p), org, font,
+                                                 fontScale, color, thickness, cv2.LINE_AA)
+
+                        thresh_img = cv2.drawContours(thresh_img, [pts], -1, (0, 255, 0), 1, cv2.LINE_AA)
+
+                work_r_points = [np.array(([self.work_zone[0], self.work_zone[1]],
+                          [self.work_zone[2], self.work_zone[1]],
+                          [self.work_zone[2], self.work_zone[3]],
+                          [self.work_zone[0], self.work_zone[3]]
+                          ))]
+                thresh_img = cv2.drawContours(thresh_img, work_r_points,
+                                              -1, (0, 255, 0), 1, cv2.LINE_AA)
 
                 #print("width ", self.kalman.estimate)
                 self.width_change.emit(self.kalman.estimate)
